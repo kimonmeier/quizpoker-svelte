40d1da1966a7341380ff1d54e6c43ce90585c028
Kimon Meier <kimon.meier@gmail.com>
2024-08-11 05:21:37 +0200

    Fehler bei den Steuerungen behoben

--- src/client/lib/components/controls/PlayerControlsModal.svelte
+++ src/client/lib/components/controls/PlayerControlsModal.svelte
@@ -125,7 +125,7 @@
 		<div class="flex flex-row w-full gap-5">
 			<button
 				class="disabled:bg-gray-700 bg-yellow-400 rounded-2xl flex-grow p-2"
-				disabled={(currentChips?.bet ?? 0) == $minimumBet - 50 || !hasControl}
+				disabled={(currentChips?.bet ?? 0) != $minimumBet - 50 || !hasControl}
 				on:click={check}
 			>
 				Check
