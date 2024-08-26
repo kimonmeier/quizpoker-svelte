37263420e20a8d7a92c350183499a7ce1f4c3e99
Kimon Meier <kimon.meier@gmail.com>
2024-08-26 17:20:40 +0200

    Fehler behoben bei welchem der Status des Spielers nicht richtig angezeigt wurde

--- src/client/lib/components/player/PlayerEntry.svelte
+++ src/client/lib/components/player/PlayerEntry.svelte
@@ -19,18 +19,16 @@
 <div
 	class="{cssClass} grid grid-cols-3 gap-3 justify-center items-center rounded-b-md h-14 bg-[#1e699c]"
 >
-	{#key player.playerStatus}
-		<PlayerEntryChips
-			{chips}
-			class="flex-grow {player.playerStatus === MemberStatus.PLEITE
-				? 'rounded-none'
-				: 'rounded-r-lg'}"
-		/>
-		{#if player.playerStatus !== MemberStatus.PLEITE}
-			<PlayerEntrySchaetzung {schaetzung} />
-			<PlayerEntryBet chipEntry={chips} memberStatus={player.playerStatus} />
-		{/if}
-	{/key}
+	<PlayerEntryChips
+		{chips}
+		class="flex-grow {player.playerStatus === MemberStatus.PLEITE
+			? 'rounded-none'
+			: 'rounded-r-lg'}"
+	/>
+	{#if player.playerStatus != MemberStatus.PLEITE && chips?.chips != 0}
+		<PlayerEntrySchaetzung {schaetzung} />
+		<PlayerEntryBet chipEntry={chips} memberStatus={player.playerStatus} />
+	{/if}
 </div>
 
 <style>
