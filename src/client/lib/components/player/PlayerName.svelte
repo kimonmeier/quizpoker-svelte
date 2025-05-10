c33bd414401165e049fb6f761f0b4d120505b0e7
Kimon Meier <kimon.meier@gmail.com>
2025-05-10 18:31:09 +0200

    Removed redundant font-serif class from PlayerName component for cleaner styling

--- src/client/lib/components/player/PlayerName.svelte
+++ src/client/lib/components/player/PlayerName.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="content w-full">
-	<div class="text-white text-center text-4xl font-bold p-2 italic font-serif text-pretty">
+	<div class="text-white text-center text-4xl font-bold p-2 italic text-pretty">
 		{name}
 	</div>
 </div>
